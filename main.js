6beb7f9b4e3cbb35bde985dd49fa372622883e4e
SarikViper <dzamolovsardor5@gmail.com>
2025-01-28 21:28:52 +0500

    added log

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ const users = [];
 Form.onsubmit = (e) => {
     e.preventDefault();
     const user = {};
-
+    // const fm = new FormData(Form)
+    // fm.forEach((key , value) => {
+    // user[key] = value
+    // })
     Required.forEach((input) => {
         user[input.name] = input.value.trim();
 });
 
 users.push(user)
 console.log(users);
-
-
 
 }
