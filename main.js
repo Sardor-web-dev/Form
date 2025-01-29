ccf4351e6015eab3631d6a15e88958aa99ea45af
SarikViper <dzamolovsardor5@gmail.com>
2025-01-29 14:23:43 +0500

    some changes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ Form.onsubmit = (e) => {
     const user = {};
     // const fm = new FormData(Form)
     // fm.forEach((key , value) => {
-    // user[key] = value
+    // user[value] = key
     // })
     Required.forEach((input) => {
-        user[input.name] = input.value.trim();
+        user[input.name] = input.value;
 
 });
 
